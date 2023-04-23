a08f1b38e528ee016ff03abe550554b72d6cee4e
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-22 22:00:28 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,27 +9,32 @@
 int main(void)
 {
     srand(time(0));
-    std:: string arq = "ASCII.txt", senha[30];
-    std:: ifstream file(arq);
+    std::string arq = "ASCII.txt";
+    char senha[TAM_SENHA];
+    std::ifstream file(arq);
 
-    if(!(file.is_open())) {
-        std:: cerr << "Houve um erro ao abrir o arquivo \""<< arq << "\"" << std:: endl;
+    if (!file.is_open()) {
+        std::cerr << "Houve um erro ao abrir o arquivo \"" << arq << "\"" << std::endl;
         return 1;
-     }
+    }
 
     std::string opcoes, linha;
 
-    while(std::getline(file, linha))
+    while (std::getline(file, linha))
     {
         opcoes += linha;
     }
 
-    for(int i = 0; i < TAM_SENHA; i++)
+    for (int i = 0; i < TAM_SENHA; i++)
     {
-        senha[i] += (opcoes[rand() % opcoes.size()]);
+        senha[i] = opcoes[rand() % opcoes.size()];
     }
 
-    std::cout << senha << std::endl;
+    for (int i = 0; i < TAM_SENHA; i++)
+    {
+        std::cout << senha[i];
+    }
+    std::cout << std::endl;
 
     return 0;
 }
