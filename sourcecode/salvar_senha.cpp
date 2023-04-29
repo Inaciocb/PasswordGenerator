da0f47234b75497971182e2b9a5abf89b013edc1
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-29 07:56:12 -0300

    Update salvar_senha.cpp

--- sourcecode/salvar_senha.cpp
+++ sourcecode/salvar_senha.cpp
@@ -4,21 +4,14 @@
 using namespace std;
 
 int main() {
-    // Abrir o arquivo
+    
     ofstream arquivo("senhas_salvas/senha.txt", ios::out | ios::app);
     
-    // Verificar se o arquivo foi aberto corretamente
     if (arquivo.is_open()) {
-        // Escrever a senha no arquivo
         arquivo << "Senha: " << senha_gerada << endl;
-
-        // Fechar o arquivo
         arquivo.close();
-
-        // Exibir mensagem de sucesso
         cout << "Senha salva com sucesso!" << endl;
     } else {
-        // Exibir mensagem de erro caso não seja possível abrir o arquivo
         cout << "Erro ao abrir o arquivo!" << endl;
     }
 
