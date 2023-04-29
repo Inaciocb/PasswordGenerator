b506288022f6d260230163b1bf01275109b17857
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-28 22:55:34 -0300

    Update interface.py

--- sourcecode/interface.py
+++ sourcecode/interface.py
@@ -7,9 +7,9 @@ import tkinter.messagebox
 import platform
 
 
-# definir paleta de cores
-bg_color = '#183632' # verde escuro
-fg_color = '#F4F4F4' # cinza claro
+
+bg_color = '#183632'
+fg_color = '#F4F4F4' 
 button_color = '#387780' 
 button_hover_color = '#007E33' 
 text_color = '#F4F4F4' 
@@ -20,12 +20,10 @@ root.geometry("400x250")
 root.config(bg=bg_color)
 
 
-font_path = "./urbani.ttf" # caminho do arquivo da fonte
+font_path = "./urbani.ttf" 
 custom_font = font.Font(family="Urbani", size=20, weight="bold")
 custom_font['family'] = font.Font(family="Urbani", name='custom_font')
 
-
-# definir estilo dos botões
 button_style = {
     'font': ('Urbani', 12, 'bold'),
     'fg': fg_color,
@@ -38,7 +36,6 @@ button_style = {
     'relief': 'flat',
 }
 
-# definir estilo das labels
 label_style = {
     'font': custom_font,
     'fg': text_color,
@@ -59,7 +56,6 @@ if (platform.system() == 'Windows' or 'Linux'):
 else:
     print("ERRO: SISTEMA OPERACIONAL DESCONHECIDO.")
 
-
 def gerar_senha():
     global senha_gerada
     senha = subprocess.check_output("../bin/./gerador")
@@ -67,39 +63,28 @@ def gerar_senha():
     lbl_senha.config(text=senha_str)
     senha_gerada = senha_str
 
-
 def copiar_senha():
     global senha_gerada
     if senha_gerada != "":
         root.clipboard_clear()
         root.clipboard_append(senha_gerada)
 
-
 def limpar_senha():
     lbl_senha.config(text="")
     senhaLabel.config(text="")
     global senha_gerada
     senha_gerada = ""
-   
 
 def salvar_senha(senha):
-    # Define o caminho absoluto para a pasta 'senhas_salvas'
     diretorio = os.path.abspath('../senhas_salvas')
-    # Verifica se o diretório 'senhas_salvas' existe, se não existir, cria
     if not os.path.exists(diretorio):
         os.makedirs(diretorio)
-    # Cria o nome do arquivo com base na quantidade de arquivos já existentes
     arquivos_salvos = len(os.listdir(diretorio))
     nome_arquivo = f'senha{arquivos_salvos+1}.txt'
-    # Escreve a senha gerada no arquivo
     with open(os.path.join(diretorio, nome_arquivo), 'w') as arquivo:
         arquivo.write(senha)
-    # Exibe uma mensagem para o usuário
     tkinter.messagebox.showinfo('Senha Salva', f'Senha salva em {os.path.join(diretorio, nome_arquivo)}')
-
-
-
-
+    
 def salvar_senha_cpp(senha):
     subprocess.call(["./salvar_senha.exe", senha])
 
@@ -108,10 +93,7 @@ def salvar_em_txt():
     if senha_gerada != "":
         salvar_senha(senha_gerada)
         salvar_senha_cpp(senha_gerada)
-
-
-
-# criar os botões
+        
 button_frame = Frame(root, bg=bg_color)
 button_frame.pack(expand=True)
 
